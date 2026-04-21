908377560a03a05d74ad7bb17d84f21431f080bf
Benoît Havret <benoit.havret@ekino.com>
2026-04-21 13:47:32 +0200

    chore: prepare v1.2.4 release

--- docs/.vitepress/versions.data.ts
+++ docs/.vitepress/versions.data.ts
@@ -2,15 +2,15 @@ export default {
   load() {
     return {
       // Core versions
-      metalastic: '1.2.3',
+      metalastic: '1.2.4',
       ksp: '2.3.6',
       kotlin: '2.3.20',
 
       // DSL versions
       dsl: {
-        rolling: '1.2.3',      // Base artifact (6.0.x currently)
-        frozen55: '1.2.3',     // 5.4-5.5 frozen
-        frozen53: '1.2.3'      // 5.0-5.3 frozen
+        rolling: '1.2.4',      // Base artifact (6.0.x currently)
+        frozen55: '1.2.4',     // 5.4-5.5 frozen
+        frozen53: '1.2.4'      // 5.0-5.3 frozen
       },
 
       // Spring Data ES versions (brought transitively by DSL artifacts)
